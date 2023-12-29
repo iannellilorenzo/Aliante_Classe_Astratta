bcfa342616054047740ea345e9ed622246e4316f
Iannelli Lorenzo <iannelli.lorenzo.studente@itispaleocapa.it>
2023-12-29 15:06:18 +0100

    struttura classe Cerchione

--- Aliante_Classe_Astratta/Cerchione.cs
+++ Aliante_Classe_Astratta/Cerchione.cs
@@ -6,8 +6,81 @@ using System.Threading.Tasks;
 
 namespace Aliante_Classe_Astratta
 {
-    public class Cerchione
+    public class Cerchione : Component
     {
+        private double _pollici;
+        private string _materiale;
 
+        public double Pollici
+        {
+            get { return _pollici; }
+            set
+            {
+                if (value >= 15F && value <= 20)
+                    _pollici = value;
+                else
+                    _pollici = 16F;
+            }
+        }
+
+        public string Materiale
+        {
+            get { return _materiale; }
+            set { _materiale = value; }
+        }
+
+        public Cerchione()
+        {
+            Pollici = 0F;
+            Materiale = string.Empty;
+        }
+
+        public Cerchione(double pollici, string materiale)
+        {
+            Pollici = pollici;
+            Materiale = materiale;
+        }
+
+        public Cerchione(Cerchione oldCerchione)
+        {
+            Pollici = oldCerchione.Pollici;
+            Materiale = oldCerchione.Materiale;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Cerchione) || obj == null)
+            {
+                return false;
+            }
+
+            Cerchione other = (Cerchione)obj;
+            return Pollici == other.Pollici && Materiale == other.Materiale;
+        }
+
+        public override void Aggiunta(Component component)
+        {
+
+        }
+
+        public override bool Rimuovi(Component component)
+        {
+            return false;
+        }
+
+        public override Component GetChild(int index)
+        {
+            return null;
+        }
+
+        public override string ToString()
+        {
+            return $"Pollici: {Pollici}; Wheel rim material: {Materiale}";
+        }
+
+        public override double Prezzo()
+        {
+            return Pollici * 1.5F;
+        }
     }
 }
