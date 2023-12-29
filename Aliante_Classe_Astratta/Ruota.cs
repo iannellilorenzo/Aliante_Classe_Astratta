d3eefe4f03a53fdc2e0198e4c793137e17c51f65
Iannelli Lorenzo <iannelli.lorenzo.studente@itispaleocapa.it>
2023-12-29 15:08:54 +0100

    struttura classe Ruota

--- Aliante_Classe_Astratta/Ruota.cs
+++ Aliante_Classe_Astratta/Ruota.cs
@@ -6,8 +6,75 @@ using System.Threading.Tasks;
 
 namespace Aliante_Classe_Astratta
 {
-    public class Ruota
+    public class Ruota : Component
     {
+        private Gomma _gomma;
+        private Cerchione _cerchione;
 
+        public Gomma Gomma
+        {
+            get { return _gomma; }
+            set { _gomma = value; }
+        }
+
+        public Cerchione Cerchione
+        {
+            get { return _cerchione; }
+            set { _cerchione = value; }
+        }
+
+        public Ruota()
+        {
+            Gomma = new Gomma();
+            Cerchione = new Cerchione();
+        }
+
+        public Ruota(Gomma wheelTire, Cerchione wheelRim)
+        {
+            Gomma = wheelTire;
+            Cerchione = wheelRim;
+        }
+
+        public Ruota(Ruota oldRuota)
+        {
+            Gomma = oldRuota.Gomma;
+            Cerchione = oldRuota.Cerchione;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Ruota) || obj == null)
+            {
+                return false;
+            }
+
+            Ruota other = (Ruota)obj;
+            return Gomma == other.Gomma && Cerchione == other.Cerchione;
+        }
+
+        public override void Aggiunta(Component component)
+        {
+
+        }
+
+        public override bool Rimuovi(Component component)
+        {
+            return false;
+        }
+
+        public override Component GetChild(int index)
+        {
+            return null;
+        }
+
+        public override string ToString()
+        {
+            return $"Ruota tire details: {Gomma}; Ruota rim details: {Cerchione}";
+        }
+
+        public override double Prezzo()
+        {
+            return 0;
+        }
     }
 }
